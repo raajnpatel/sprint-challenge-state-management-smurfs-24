f6dd34a35eea3e779ab598bcbd4f66fc2461f0f2
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-08 08:50:41 -0800

    actions/index created

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const FETCH_SMURFS_DATA_START = "FETCH_SMURFS_DATA_START";
+export const FETCH_SMURFS_DATA_SUCCESS = "FETCH_SMURFS_DATA_SUCCESS";
+export const FETCH_SMURFS_DATA_FAILURE = "FETCH_SMURFS_DATA_FAILURE";
+
+export const getData = () => {
+    return dispatch => {
+        dispatch({ type: FETCH_SMURFS_DATA_START });
+        axios
+            .get(`http://localhost:3333/smurfs`)
+            .then(res => {
+                console.log(res);
+                dispatch({ type: FETCH_SMURFS_DATA_SUCCESS, payload: res });
+            })
+            .catch(err => {
+                dispatch({ type: FETCH_SMURFS_DATA_FAILURE, payload: err });
+            });
+    };
+};
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -7,4 +7,10 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { reducer } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store = {store}>
+    <App />
+    </Provider>,
+    document.getElementById("root"));
