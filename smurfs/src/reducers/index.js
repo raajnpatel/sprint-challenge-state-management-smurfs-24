56fd37ab36c9a4294942cda9292554894e5f8927
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-08 08:28:30 -0800

    actions and reducer folder/index created

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    smurfs:[],
+    error:""
+};
+
+export const reducer = (state=initialState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+};
