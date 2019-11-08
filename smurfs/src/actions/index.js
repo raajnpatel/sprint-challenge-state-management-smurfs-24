e995b1c954a36c872a12a59d140fcc7bd61649a1
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-08 09:58:14 -0800

    actions built for put/delete
    stretch complete

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -32,4 +32,41 @@ export const addSmurf = newSmurf => dispatch => {
         });
 };
 
+export const DELETE_START = "DELETE_START";
+export const DELETE_SUCCESS = "DELETE_SUCCESS";
+export const DELETE_ERROR = "DELETE_ERROR";
 
+export const deleteSmurf = id => dispatch => {
+    dispatch({ type: DELETE_START });
+    axios.delete(`http://localhost:3333/smurfs/${id}`)
+        .then(res => {
+            dispatch({ type: DELETE_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            dispatch({ type: DELETE_ERROR, payload: err.response.status });
+        })
+};
+
+export const SET_SMURF_TO_EDIT = "SET_SMURF_TO_EDIT";
+
+export const setSmurfToEdit = smurf => {
+    return { type: SET_SMURF_TO_EDIT, payload: smurf }
+};
+
+export const PUT_START = "PUT_START";
+export const PUT_SUCCESS = "PUT_SUCCESS";
+export const PUT_ERROR = "PUT_ERROR";
+
+export const editSmurf = smurf => dispatch => {
+    dispatch({ type: PUT_START });
+    axios.put(`http://localhost:3333/smurfs/${smurf.id}`, smurf)
+        .then(res => {
+            dispatch({ type: PUT_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            dispatch({ type: PUT_ERROR, payload: err.response.status });
+        })
+        .finally(() => {
+            dispatch(setSmurfToEdit(null));
+        })
+};
